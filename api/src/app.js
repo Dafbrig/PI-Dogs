ce23445dae2698ef4a0cc9a645500e14649066cf
David Briceño <61285906+Dafbrig@users.noreply.github.com>
2023-08-18 14:52:30 -0500

    Mejora y comentarios

--- api/src/app.js
+++ api/src/app.js
@@ -2,35 +2,33 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const routes = require('./routes/index.js');
-const dogsRoutes = require('./routes/dogs'); // Importa las rutas para dogs
-const temperamentsRoutes = require('./routes/temperaments'); // Importa las rutas para temperaments
+const routes = require('./routes/index.js'); // Importa las rutas principales
 
-require('./db.js');
-require('dotenv').config(); // Carga las variables de entorno desde .env
+require('./db.js'); // Importa la configuración de la base de datos
 
 const server = express();
 
 server.name = 'API';
 
+// Configuración de middlewares
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
+
+// Configuración de CORS para permitir peticiones desde el frontend
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Cambia esto para que coincida con tu dominio de frontend
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
 
-// Usa las rutas para los diferentes recursos
+// Configuración de rutas principales
 server.use('/', routes);
-server.use('/dogs', dogsRoutes); // Conecta las rutas para dogs
-server.use('/temperaments', temperamentsRoutes); // Conecta las rutas para temperaments
 
-// Middleware para manejo de errores
+// Middleware para manejar errores
 server.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || err;
--- api/src/db.js
+++ api/src/db.js
@@ -1,40 +1,63 @@
-require('dotenv').config(); // Carga las variables de entorno desde .env
-const { Sequelize } = require('sequelize');
-const fs = require('fs');
-const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env;
+// Carga las variables de entorno desde .env
+require('dotenv').config();
 
-// Crea la instancia de Sequelize con la URL de conexión
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
-  logging: false, // Establece en "false" para no mostrar las consultas SQL en la consola
-  native: false, // Desactiva pg-native para mayor velocidad
+// Importa las dependencias necesarias
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+// Importa las rutas para los diferentes recursos
+const routes = require('./routes/index.js');
+const dogsRoutes = require('./routes/dogs'); // Importa las rutas para dogs
+const temperamentsRoutes = require('./routes/temperaments'); // Importa las rutas para temperaments
+
+// Importa la conexión a la base de datos y modelos
+const { conn } = require('./db.js');
+
+// Crea una instancia de Express
+const server = express();
+
+// Asigna un nombre a tu servidor
+server.name = 'API';
+
+// Configura middleware para el manejo de solicitudes y respuestas
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
+server.use(cookieParser());
+server.use(morgan('dev'));
+server.use((req, res, next) => {
+  // Configura encabezados CORS para permitir solicitudes desde el frontend
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
 });
 
-const basename = path.basename(__filename);
+// Usa las rutas para los diferentes recursos
+server.use('/', routes);
+server.use('/dogs', dogsRoutes); // Conecta las rutas para dogs
+server.use('/temperaments', temperamentsRoutes); // Conecta las rutas para temperaments
 
-const modelDefiners = [];
+// Middleware para manejo de errores
+server.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
 
-// Lee todos los archivos en la carpeta Models y los requiere, luego los agrega al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/models'))
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
-  .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+// Sincroniza la base de datos y arranca el servidor
+conn.sync({ force: false }) // Cambia a `true` si deseas que las tablas se recreen en cada reinicio
+  .then(() => {
+    server.listen(3001, () => {
+      console.log('Server is listening on port 3001');
+    });
+  })
+  .catch((error) => {
+    console.error('Error syncing database:', error);
   });
 
-// Injecta la conexión (sequelize) en todos los modelos
-modelDefiners.forEach(model => model(sequelize));
-
-// Capitaliza los nombres de los modelos (por ejemplo, dog => Dog)
-let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
-sequelize.models = Object.fromEntries(capsEntries);
-
-// Aquí podrían agregarse relaciones entre los modelos, como Product.hasMany(Reviews)
-
-// Exporta los modelos y la conexión para poder importarlos en otros archivos
-module.exports = {
-  ...sequelize.models, // Para importar los modelos así: const { Dog, Temperament } = require('./db.js');
-  conn: sequelize,     // Para importar la conexión así: const { conn } = require('./db.js');
-};
+// Exporta el servidor para usarlo en otros archivos
+module.exports = server;
